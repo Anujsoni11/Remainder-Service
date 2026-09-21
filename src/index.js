1e9227f3aec507b02a5f3fca0eae1f724d297f10
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-21 16:38:57 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 const { sendBasicEmail } = require('./services/email-service')
+// const {sendBasicEmail}=require('./services/email-service')
 
 const setupAndStartServer = () => {
     const app = express();
@@ -13,12 +14,16 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`server started on port : ${PORT}`);
-        sendBasicEmail(
-            'support@admin.com',
-            'cs191297@gmail.com',
-            'this is a testing email',
-            'Hey, how are you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'cs191297@gmail.com',
+        //     'this is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // )
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 }
 setupAndStartServer();
