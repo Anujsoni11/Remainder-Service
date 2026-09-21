482738af2b21ded31bc31d7f190b7948dd104f9c
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-21 16:33:27 +0530

    Setup of SMTP using nodemailer done

---
+++ src/config/emailConfig.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer');
+
+const { EMAIL_ID, EMAIL_PASSWORD } = require('./serverConfig');
+
+const sender = nodemailer.createTransport({
+    service: 'GMAIL',
+    auth: {
+        user: EMAIL_ID,
+        pass: EMAIL_PASSWORD
+    }
+});
+
+module.exports = sender;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service')
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -11,6 +13,12 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`server started on port : ${PORT}`);
+        sendBasicEmail(
+            'support@admin.com',
+            'cs191297@gmail.com',
+            'this is a testing email',
+            'Hey, how are you, I hope you like the support'
+        )
     });
 }
 setupAndStartServer();
